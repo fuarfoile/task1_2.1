f62b75bbd220a996a2eb5b123b9ee10a67b3505a
Oleh Bondarenko <boa.st@ya.ru>
2017-07-25 19:01:31 +0300

    Update Main.java

--- src/com/boast/task2/Main.java
+++ src/com/boast/task2/Main.java
@@ -1,8 +1,7 @@
 package com.boast.task2;
 
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Random;
+import com.boast.Matrix;
 
 /**
  * Created by Bondarenko Oleh on 22.07.2017.
@@ -12,42 +11,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int dimensions = 7;
+        int matrixSize = 7;
 
         Scanner in = new Scanner(System.in);
         int k = in.nextInt();
 
-        int[][]matrix = new int[dimensions][dimensions];
+        Matrix matrix = new Matrix(matrixSize, matrixSize);
 
-        matrixGenerate(matrix);
-        matrixShow(matrix);
+        matrix.generate();
+        matrix.show();
         System.out.println();
-        matrixShift(matrix, k);
-        matrixShow(matrix);
-
-    }
-
-    private static void matrixGenerate(int[][] matrix) {
-        Random rand = new Random();
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = rand.nextInt(200) - 100;
-            }
-        }
-    }
-
-    private static void matrixShow(int[][] matrix) {
-        for(int i = 0; i < matrix.length; i++) {
-            System.out.println(Arrays.toString(matrix[i]));
-        }
-    }
-
-    private static void matrixShift(int[][] matrix, int shift) {
-        for(int i = 0; i < matrix.length; i++) {
-            int[] helpMatrix = Arrays.copyOf(matrix[i], matrix[i].length);
-            for(int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = helpMatrix[(j + shift) % helpMatrix.length];
-            }
-        }
+        matrix.leftShift(k);
+        matrix.show();
     }
 }
