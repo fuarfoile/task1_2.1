0595dfd0e20737733873dab1274f0774866eaa2b
Oleh Bondarenko <boa.st@ya.ru>
2017-07-27 00:20:27 +0300

    Add files via upload

---
+++ src/com/boast/task2/Main.java
@@ -0,0 +1,27 @@
+package com.boast.task2;
+
+import java.util.Scanner;
+import com.boast.Matrix;
+
+/**
+ * Created by Bondarenko Oleh on 22.07.2017.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int matrixSize = 7;
+
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+
+        Matrix matrix = new Matrix(matrixSize, matrixSize);
+
+        matrix.generate();
+        matrix.show();
+        System.out.println();
+        matrix.leftShift(k);
+        matrix.show();
+    }
+}
